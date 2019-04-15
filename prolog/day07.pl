ab9482833ae72af9e54bd6bf614afaddbd646502
dustinheestand <heestand@post.harvard.edu>
2019-04-15 11:30:40 -0500

    currently looping finding and re-adding an unoccupied elf

--- prolog/day07.pl
+++ prolog/day07.pl
@@ -2,7 +2,7 @@
 :- use_module(library(clpfd)).
 :- use_module(library(dcg/basics)).
 :- op(920, fy, *).
-:- (dynamic dep/2).
+:- (dynamic prereq_dependent/2).
 
 % Parse my file!
 dependencies([E|Rest]) -->
@@ -12,7 +12,7 @@ dependencies([E|Rest]) -->
 dependencies([E]) -->
     dependency(E). 
 % A dependency has this form
-dependency(dep(A, B)) -->
+dependency(prereq_dependent(A, B)) -->
     "Step ",
     step(A),
     " must be finished before step ",
@@ -31,3 +31,80 @@ load_funct(Filename) :-
     forall(member(Dep, D), assertz(Dep)),
     !.
 
+% Now to actually solve the problem
+solve(Ordering) :-
+    findall(Prereq, prereq_dependent(Prereq, _), Prereqs),
+    findall(Dependent, prereq_dependent(_, Dependent), Dependents),
+    ord_union(Prereqs, Dependents, Tasks),
+    sort(Tasks, SortedTasks),
+    elves_init(5, Elves),
+    phrase(solve(Elves, [], SortedTasks, []), Ordering).
+
+elves_init(0, []).
+elves_init(N, [(0, 0)|Es]) :-
+    N#>0,
+    N1#=N-1,
+    elves_init(N1, Es).
+
+% In the case where there is an open slot and an eligible task
+% to fill it.
+solve(Elves, InProgress, ToDo, Completed) -->
+    { select((FinishedTask, 0), Elves, Elves1),
+      member(Task, ToDo),
+      % Inelegant - I'd like to take the complement of Completed
+      append(InProgress, ToDo, NotCompleted),
+      \+ dependent(NotCompleted, Task),
+      select(Task, ToDo, ToDo1)
+    },
+    [Task],
+    solve([(Task, Task)|Elves1],
+          [Task|InProgress],
+          ToDo1,
+          [FinishedTask|Completed]).
+
+solve(Elves, InProgress, ToDo, Completed) -->
+    { append(InProgress, ToDo, NotCompleted),
+      % If all the tasks not yet completed are dependent on ToDos
+      (   maplist(dependent(NotCompleted), ToDo)
+      ;   maplist(has_time_left, Elves)
+      ),
+      subtract_time(Elves, Elves1)
+    },
+    [],
+    solve(Elves1, InProgress, ToDo, Completed).
+
+solve(_, [], [], _) -->
+    [].
+
+dependent(Prereqs, Task) :-
+    member(Prereq, Prereqs),
+    prereq_dependent(Prereq, Task).
+
+% Works
+subtract_time(Elves, Elves1) :-
+    min_time_left(Elves, Min),
+    maplist(subtract_time(Min), Elves, Elves1).
+    
+% Works
+min_time_left(Elves, N) :-
+    maplist(get_time_left, Elves, Times),
+    min_list(Times, N).
+
+% Works
+get_time_left((_, N), N).
+
+% Works
+subtract_time(T,  (M, N),  (M, N1)) :-
+    N1#=N-T.
+
+% Works
+has_time_left((_, N)) :-
+    N#>0.
+
+solve_test(Task) :-
+    findall(Prereq, prereq_dependent(Prereq, _), Prereqs),
+    findall(Dependent, prereq_dependent(_, Dependent), Dependents),
+    union(Prereqs, Dependents, Tasks),
+    sort(Tasks, SortedTasks),
+    member(Task, SortedTasks),
+    \+ prereq_dependent(_, Task).
